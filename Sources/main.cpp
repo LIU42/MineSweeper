972612cfbada27b4a6410d6b3591c29505e7cefa
LIU42 <3528865430@qq.com>
2023-08-21 23:17:58 +0800

    设置高 DPI 缩放规则。

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,7 +1,14 @@
-﻿#include "mainwindow.h"
+#include "mainwindow.h"
+
+void setHighDPIScalePolicy()
+{
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::Floor);
+}
 
 int main(int argc, char* argv[])
 {
+    setHighDPIScalePolicy();
+
     QApplication app(argc, argv);
     MainWindow window;
     MainGame game;
